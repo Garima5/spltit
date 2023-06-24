1b60d97d0bb0beb99d2e9369f0b0c7d083332f74
Garima Aggarwal <garimaaggarwal@Garimas-MacBook-Pro.local>
2023-06-24 17:29:38 -0400

    Creating the Expense Structure

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,3 +5,19 @@ pragma solidity ^0.8.6;
 // Event Id - unique generate
 // Assign Vendor
 // For each contributor - add the percentage that they are offering
+contract Splitit{
+    uint256 tempId = 0;
+    struct Expense{
+        bytes32 expenseId;
+        // string expenseDataCID;
+        address expenseOwner;
+        address expenseVendor;
+        uint256 deadline; // deadline upto which users can contribute
+        bool paidOut; // If paidOut - then no new person can add their contribution
+        uint256 initialDeposit;
+        uint256 maxCapacity;
+        uint256 actualCost;
+        address[] confirmedContributors;
+        address[] paidOutContributor; // list of contributors who have been paid theor deposit back
+        // where to store each contributor's deposit value?
+    }
