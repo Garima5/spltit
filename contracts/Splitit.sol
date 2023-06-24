4fc515244aa2399becac934bbb256c103ec32f13
Garima Aggarwal <garimaaggarwal@Garimas-MacBook-Pro.local>
2023-06-24 17:55:06 -0400

    Fixed the function to create new expense

--- contracts/Splitit.sol
+++ contracts/Splitit.sol
@@ -23,5 +23,38 @@ contract Splitit{
     }
     mapping (address=> uint256) amountsDeposited; // amount deposited per person 
     mapping (bytes32 => Expense) public idToExpense;
+    // function to create new expense
+    function createNewExpense(
+        address _vendor,
+        uint256 _deadline,
+        uint256 _initialDeposit,
+        uint256 _maxCapacity,
+        uint256 actualAmount
+    ) external{
+        // later -> generate event id
+        address[] memory contributors;
+        address[] memory _paidOutContributor; 
+        bytes32 eventId = keccak256(
+        abi.encodePacked(
+            msg.sender,
+            _deadline,
+            _initialDeposit,
+            _maxCapacity
+        )
+    );
+    //mapping(address=> uint256) _amountsDeposited;
+
+        idToExpense[eventId]=Expense(
+            eventId,
+            msg.sender,
+            _vendor,
+            _deadline,
+            false,
+            _initialDeposit,
+            _maxCapacity,
+            actualAmount,
+            contributors,
+            _paidOutContributor);
+    }
     
 }
